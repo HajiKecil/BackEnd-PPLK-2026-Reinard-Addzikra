cc87bd6f00cdaba3354c93aaf2b6b256b9b32178
Reinard Addzikra <reinard.contact@gmail.com>
2026-06-20 13:57:15 +0700

    Myportofolio BackEnd PPLK 2026 :)

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 import sqlite3
 
-
+# Membuat aplikasi web backend menggunakan FastAPI
 app = FastAPI()
 
 def init_db():
+    # Membuka file database, kalau ngga ada filenya maka akan otomatis buat baru
     conn   = sqlite3.connect("pplk_tugas.db")
+    # Menulis perintah SQL ke database
     cursor = conn.cursor()
-
+    # Perintah membuat tabel maba (lemari arsip) kalau lemarinya belum ada
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS maba (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,45 +19,63 @@ def init_db():
             link_tugas TEXT
         )
     """)
+    # Menyimpan perubahan ke database
     conn.commit()
+    # Menutup koneksi ke database
     conn.close()
 
+# Database nya langsung dibuat pas aplikasi pertama kali dinyalakan
 init_db()
 
+# =========================================================================
+# 1. Menu Home
+# =========================================================================
 @app.get("/")
 def home():
+    # Tampilan otomatis saat aplikasi backend nya diakses lewat browser atau aplikasi frontend
     return {"message": "Sistem Back End PPLK 2026 Aktif dengan Database SQLite!"}
 
-
-
+# =========================================================================
+# 2. Menu Lihat Tugas
+# =========================================================================
 @app.get("/tugas")
-def lihat_tugas(nim_kamu: int = None):
+def lihat_tugas(nim_kamu: int = None): # Parameter opsional, kalau ngga dikasih nilai maka default nya None
     status_sekarang = "Belum mengumpulkan"
 
     conn   = sqlite3.connect("pplk_tugas.db")
     cursor = conn.cursor()
+
+    # Nyari data mahasiswa berdasarkan NIM yang dikasih lewat parameter
     cursor.execute("SELECT * FROM maba WHERE nim = ?", (nim_kamu,))
+    # Karena nim itu unik, cukup ambil teratas satu teratas yang ketemu
     data   = cursor.fetchone()
     conn.close()
 
+    # logika kalau data nya ada, maka statusnya "Anda sudah mengumpulkan" 
     if data:
-        status_sekarang = "Gile lu ! jago banget udah ngumpulin"
+        status_sekarang = "Anda sudah mengumpulkan"
 
+    # Info dan hasil statusnya ditampilkan ke browser
     return {
     "nama_tugas": "Resume Perkenalan",
-    "Deadline": "22 July 2026",
+    "Deadline": "21 Juni 2026",
     "status": status_sekarang
 }
 
-@app.post("/kumpul")
+# =========================================================================
+# 3. Menu Mengumpulkan Tugas
+# =========================================================================
+@app.post("/kumpul") # <-- menggunakan ".post " karena orang yang mengumpulkan akan memasukkan data, bukan hanya menampilkan tampilan bawaan ".get"
 def kumpul_tugas(nama: str, nim: int, kelompok: int, link_tugas: str):
     conn   = sqlite3.connect("pplk_tugas.db")
     cursor = conn.cursor()
+
+    # Memasukkan data yang dikirim baru baru itu ke dalam tabel
     cursor.execute("""
         INSERT INTO maba (nama, nim, kelompok, link_tugas)
         VALUES (?, ?, ?, ?)
     """, (nama, nim, kelompok, link_tugas))
-    conn.commit()
+    conn.commit() # Di commit ini biar data orang yang baru mengumpulkan itu ngga hilang
     conn.close()
 
     return {
@@ -63,30 +83,43 @@ def kumpul_tugas(nama: str, nim: int, kelompok: int, link_tugas: str):
         "pesan": f"Halo {nama} ({nim}), tugas kelompok {kelompok} berhasil dikumpulkan ke dalam database SQLite kelompok {kelompok}!"
     }
     
-
+# =========================================================================
+# 4. Menu Rekap Total
+# =========================================================================
 @app.get("/rekap-tugas")
 def lihat_semua_tugas():
     conn = sqlite3.connect("pplk_tugas.db")
-    # Hasil otomatis jadi format JSON/Dictionary 
+
+    # Data otomatis dibungkus rapi lengkap dengan judul kolomnya
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
+
+    # Mengambil semua data maba tanpa terkecuali dari database
     cursor.execute("SELECT * FROM maba")
     rows = cursor.fetchall()
     conn.close()
 
+    # Me-looping buat ngubah semua baris data mentah tadi jadi format Dictionary/JSON (JavaScript Object Notation)
     semua_data =[dict(row) for row in rows]
 
+    # Menampilkan total jumlah peserta beserta daftar lengkap datanya
     return {"total_mengumpulkan": len(semua_data), "data": semua_data}
 
+# =========================================================================
+# 5. Menu Rekap Per Kelompok (Fitur khusus panitia)
+# =========================================================================
 @app.get("/cek-kelompok/{nomor_kelompok}")
 def cek_per_kelompok(nomor_kelompok: int):
     conn = sqlite3.connect("pplk_tugas.db")
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
+
+    # Mengambil hanya baris mahasiswa yang nomer kelompoknya cocok 
     cursor.execute("SELECT * FROM maba WHERE kelompok = ?", (nomor_kelompok,))
     rows = cursor.fetchall()
     conn.close()
     
+    # Dibungkus lagi jadi JSON biar rapi pas dibaca panitia / frontend
     hasil_filter =[dict(row) for row in rows]
 
     return {
